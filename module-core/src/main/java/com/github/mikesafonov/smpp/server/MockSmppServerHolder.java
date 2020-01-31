20c5a49b1443537d20fe9ef83540a5eccec11020
msafonov <msafonovmail@gmail.com>
2020-01-31 11:17:56 +0300

    chore: added javadoc to MockSmppServerHolder

--- module-core/src/main/java/com/github/mikesafonov/smpp/server/MockSmppServerHolder.java
+++ module-core/src/main/java/com/github/mikesafonov/smpp/server/MockSmppServerHolder.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 
 /**
+ * Holder class for list of {@link MockSmppServer}
+ *
  * @author Mike Safonov
  */
 @Slf4j
@@ -15,15 +17,20 @@ import java.util.List;
 public class MockSmppServerHolder {
     private final List<MockSmppServer> servers;
 
+    /**
+     * Starts all {@link MockSmppServer}
+     */
     public void startAll() {
         for (MockSmppServer server : servers) {
             log.info("Starting " + server.getDescription());
             server.start();
             log.info(server.getDescription() + " started");
-
         }
     }
 
+    /**
+     * Stops all {@link MockSmppServer}
+     */
     public void stopAll() {
         for (MockSmppServer server : servers) {
             log.info("Stopping " + server.getDescription());
